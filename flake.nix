5290fe0a0eb4400b8f716c1f2300e81c16f19d27
Paul Meyer <katexochen0@gmail.com>
2026-04-15 12:08:27 +0200

    update disorderfs
    Signed-off-by: Paul Meyer <katexochen0@gmail.com>

--- flake.nix
+++ flake.nix
@@ -4,12 +4,35 @@
   outputs =
     { self, nixpkgs }:
     let
+      overlay = final: prev: {
+        disorderfs = final.stdenv.mkDerivation (finalAttrs: {
+          pname = "disorderfs";
+          version = "0.6.2";
+          src = final.fetchFromGitLab {
+            domain = "salsa.debian.org";
+            owner = "reproducible-builds";
+            repo = "disorderfs";
+            tag = "${finalAttrs.version}";
+            hash = "sha256-1ehGbNYbOewnDrQ1JhozKMvfVaCH7sDCxrD0dvwAfw0=";
+          };
+          nativeBuildInputs = with final; [
+            pkg-config
+            asciidoc-full
+          ];
+          buildInputs = with final; [
+            fuse3
+            attr
+          ];
+          NIX_CFLAGS_COMPILE = [ "-D_FILE_OFFSET_BITS=64" ];
+          installFlags = [ "PREFIX=$(out)" ];
+        });
+      };
       forAllSystems =
         function:
         nixpkgs.lib.genAttrs [
           "x86_64-linux"
           "aarch64-linux"
-        ] (system: function nixpkgs.legacyPackages.${system});
+        ] (system: function (nixpkgs.legacyPackages.${system}.extend overlay));
     in
     {
       packages = forAllSystems (pkgs: {
